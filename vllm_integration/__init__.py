d91ebfe2d1d43ce9f661b2203361117beae0b4bc
Khiem Le <87161814+Khiem17204@users.noreply.github.com>
2026-04-04 18:41:10 -0400

    feat(vllm): wrap CacheEngine with CacheGen compression

--- vllm_integration/__init__.py
+++ vllm_integration/__init__.py
@@ -4,10 +4,13 @@ from .hook import (
     PhysicalBlock,
     VllmCacheGenHook,
 )
+from .cachegen_cache_engine import CacheGenCacheEngine, wrap_cache_engine
 
 __all__ = [
     "MockVllmCacheEngine",
     "NetworkSimulator",
     "PhysicalBlock",
     "VllmCacheGenHook",
+    "CacheGenCacheEngine",
+    "wrap_cache_engine",
 ]
